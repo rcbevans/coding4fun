b8e51923b3b95980d3695bf975e9d1549e8b5b37
o0rebelious0o <rcb.evans@gmail.com>
2014-01-16 17:36:49 +0000

    Massive optimisations to Highly Divisible Triangular Number

--- src/Highly divisible triangular number.py
+++ src/Highly divisible triangular number.py
@@ -1,4 +1,5 @@
 from math import sqrt, floor
+from time import time
 
 class findDivisors:
 	def isPrime(self, num):
@@ -13,7 +14,7 @@ class findDivisors:
 		currNum = num
 		currFactor = 2
 
-		while not self.isPrime(currNum):
+		while currNum > 1:
 			if not currNum % currFactor:
 				if currFactor not in factors:
 					factors[currFactor] = 1
@@ -22,11 +23,6 @@ class findDivisors:
 				currNum = currNum / currFactor
 			else:
 				currFactor += 1
-
-		if currNum not in factors:
-			factors[currNum] = 1
-		else:
-			factors[currNum] += 1
 			
 		total = 1
 		for key in factors:
@@ -42,6 +38,64 @@ class findDivisors:
 
 		return candidate
 
+
+class findDivisors2:
+	def __init__(self):
+		self.primes = self.initPrimes(10000)
+
+	def initPrimes(self, num):
+		candidate = 3
+		primes = [2]
+		while len(primes) < num:
+			if self.isPrime(candidate):
+				primes.append(candidate)
+			candidate += 1
+		return primes
+
+	def isPrime(self, num):
+		for i in range(2, int(floor(sqrt(num)) + 1)):
+			if not num % i:
+				return False
+		return True
+
+	def primeGen(self):
+		index = 0
+		while index < len(self.primes):
+			yield self.primes[index]
+			index += 1
+
+	def factorize(self, num):
+		factors = {}
+		curr = num
+		pg = self.primeGen()
+		i = pg.next()
+		while curr > 1:
+			if not curr % i:
+				self.addfactor(factors, i)
+				curr = curr / i
+			else:
+				i = pg.next()
+		return factors
+
+	def addfactor(self, factor, n):
+		if n not in factor:
+			factor[n] = 1
+		else:
+			factor[n] += 1
+
+	def firstWithNDivisors(self, num):
+		gt = genTriangular()
+		candidate = gt.next()
+		noDivisors = -1
+		while noDivisors < num:
+			candidate = gt.next()
+			factors = self.factorize(candidate)
+			total = 1
+			for key in factors:
+				total *= factors[key] + 1
+			noDivisors = total
+		return candidate
+
 def genTriangular():
 	next = 1
 	current = 0
@@ -52,4 +106,14 @@ def genTriangular():
 
 if __name__ == '__main__':
 	fd = findDivisors()
-	print fd.firstWithNDivisors(5)
+	start = time()
+	res = fd.firstWithNDivisors(500)
+	fin = time()
+	t1 = fin - start
+	print "Took", t1, "s", res
+	fd2 = findDivisors2()
+	start = time()
+	res2 = fd2.firstWithNDivisors(500)
+	fin = time()
+	t2 = fin - start
+	print "Took", t2, "s", res
