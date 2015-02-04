41329a17a515790de8f5cdac3ba5d163e120721e
Rich Evans <rcb.evans@gmail.com>
2015-02-04 15:13:17 +0000

    added clojure solution for project euler problem 4

--- src/ProjectEuler/clojure/projecteuler.p4/src/projecteuler/p4.clj
+++ src/ProjectEuler/clojure/projecteuler.p4/src/projecteuler/p4.clj
@@ -2,13 +2,13 @@
   (:gen-class))
 
 (defn largest-pallendrome
-	[]
-	(let [three-digit-nums (range 100 1000)]
-		(->> (for [a three-digit-nums
-				   b three-digit-nums]
-				   (* a b))
-			(filter #(= (seq (str %1)) (reverse (str %1))))
-			(apply max))))
+    []
+    (let [three-digit-nums (range 100 1000)]
+        (->> (for [a three-digit-nums
+                   b three-digit-nums]
+                   (* a b))
+            (filter #(= (seq (str %1)) (reverse (str %1))))
+            (apply max))))
 
 (defn -main
   "I don't do a whole lot ... yet."
